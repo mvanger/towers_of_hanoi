ea9d2b4975d2a19082eae8b5ef75a8071893323c
mvanger <mvanger@nyu.edu>
2013-06-18 12:06:48 -0400

    mad lib

---
+++ madlibs/madlibs.rb
@@ -0,0 +1,24 @@
+puts "Give me an animal."
+noun1 = gets.chomp
+puts "Give me an adverb"
+adverb1 = gets.chomp
+puts "Give me a gerund"
+verb1 = gets.chomp
+puts "Name a place"
+place1 = gets.chomp
+puts "Give an adjective"
+adj1 = gets.chomp
+puts "Give a feeling"
+feeling1 = gets.chomp
+puts "Pronoun of the first noun?"
+pronoun1 = gets.chomp
+puts "Give a past tense verb"
+verb2 = gets.chomp
+puts "Another place?"
+place2 = gets.chomp
+puts "How about a past tense verb?"
+verb3= gets.chomp
+
+puts "The #{noun1} went #{adverb1} #{verb1} to #{place1}, which
+was #{adj1} that day. The #{noun1} was #{feeling1}, so #{pronoun1}
+#{verb2} all the way to #{place2}. There, #{pronoun1} finally #{verb3}."
