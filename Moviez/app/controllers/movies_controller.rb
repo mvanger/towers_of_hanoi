1fa14f38c65e4242473723ecd02a7a54c78ce6bd
mvanger <mvanger@nyu.edu>
2013-07-10 20:36:07 -0400

    http://cdn.memegenerator.net/instances/400x/25903230.jpg

--- Moviez/app/controllers/movies_controller.rb
+++ Moviez/app/controllers/movies_controller.rb
@@ -12,4 +12,16 @@ class MoviesController < ApplicationController
   def show
     @movies = Imdb::Search.new(params[:id]).movies.first
   end
+
+  def update
+    @movie = Movie.new
+    movie = Imdb::Search.new(params[:id]).movies.first
+    @movie.title = movie.title
+    @movie.year = movie.year
+    @movie.plot = movie.plot
+    @movie.mpaa_rating = movie.mpaa_rating
+    @movie.rating = movie.rating
+    @movie.save
+    redirect_to("/movies/#{@movie.title}")
+  end
 end
--- Moviez/config/routes.rb
+++ Moviez/config/routes.rb
@@ -2,6 +2,7 @@ Moviez::Application.routes.draw do
   root :to => 'welcome#index'
   get '/movies/search' => 'movies#search', as: 'movies_search'
   resources :movies
+  post '/movies/:title/favorite' => 'movies#favorite', as: 'favorite'
 
 
 end
