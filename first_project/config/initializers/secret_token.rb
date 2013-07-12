d069ef4736c384706964cfc05581f274607c1606
mvanger <mvanger@nyu.edu>
2013-07-11 22:05:50 -0400

    first commit

---
+++ first_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstProject::Application.config.secret_token = '32d902e23726ea222cfa243fa78d163223260574c81ce601f16f922fbaaffd1d95995653b14a810c36d1466b418f5f46ab97784eaa710f48deabe87c79d72d9c'
---
+++ first_project/db/migrate/20130712011619_tables.rb
@@ -0,0 +1,7 @@
+class Tables < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
